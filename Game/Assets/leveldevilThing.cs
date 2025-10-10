4b7042113681d2051d8245edf7344562de3d456e
Michael4Camps <C24354791@myTUDublin.ie>
2025-10-10 13:24:02 +0100

    MoveingPlatformsWork

---
+++ Game/Assets/leveldevilThing.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class leveldevilThing : MonoBehaviour
+{
+    public bool canFloorMove;
+
+    public GameObject slidingFloor;
+    public GameObject tarPosition;
+
+    //public GameObject targetPosition;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the colliding object is the player
+        if (collision.gameObject.CompareTag("PlayerOne") || (collision.gameObject.CompareTag("PlayerTwo")))
+        {
+            Debug.Log("Player entered the trigger-sliding floor");
+
+            //
+
+            canFloorMove = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerOne") || (collision.gameObject.CompareTag("PlayerTwo")))
+        {
+            Debug.Log("Player exited the trigger-sliding floor");
+
+            //
+
+            canFloorMove = false;
+        }
+
+    }
+
+
+
+    //public GameObject targetObject;
+    public float smoothingSpeed = 5f;
+
+    void Update()
+    {
+        if (canFloorMove)
+        {
+            Vector2 startPosition = slidingFloor.transform.position;
+            Vector2 endPosition = tarPosition.transform.position;
+            transform.position = Vector2.Lerp(startPosition, endPosition, smoothingSpeed * Time.deltaTime);
+        }
+    }
+}
