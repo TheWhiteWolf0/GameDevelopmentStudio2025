88c33c8ddbbc54ecdf94e3677a60d9e92521bd2b
Michael4Camps <C24354791@myTUDublin.ie>
2025-10-18 17:16:47 +0100

    Update

--- Game/Assets/Scripts/KillPlayer.cs
+++ Game/Assets/Scripts/KillPlayer.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class KillPlayer : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject playerOne;
+    public GameObject playerTwo;
     public Transform respawnPoint;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("PlayerOne"))
         {
-            player.transform.position = respawnPoint.position;
+            playerOne.transform.position = respawnPoint.position;
+        }
+
+        if (other.gameObject.CompareTag("PlayerTwo"))
+        {
+            playerTwo.transform.position = respawnPoint.position;  
         }
     }
 
--- Game/Assets/leveldevilThing.cs
+++ Game/Assets/leveldevilThing.cs
@@ -7,6 +7,7 @@ public class leveldevilThing : MonoBehaviour
     public GameObject slidingFloor;
     public GameObject tarPosition;
 
+    public Vector2 thingThing;
     //public GameObject targetPosition;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -43,11 +44,21 @@ public class leveldevilThing : MonoBehaviour
 
     void Update()
     {
+
         if (canFloorMove)
         {
             Vector2 startPosition = slidingFloor.transform.position;
             Vector2 endPosition = tarPosition.transform.position;
             transform.position = Vector2.Lerp(startPosition, endPosition, smoothingSpeed * Time.deltaTime);
+            //thing();
         }
+    }
+
+    private void thing()
+    {
+        //thingThing = new Vector2();
+        Vector2 startPosition = slidingFloor.transform.position;
+        //Vector2 thingEnd = thingThing;
+        transform.position = Vector2.Lerp(startPosition, thingThing, smoothingSpeed * Time.deltaTime);
     }
 }
