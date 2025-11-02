5496f49d36dd9c53fb59d2c8c1d0a57791d2e360
Michael4Camps <C24354791@myTUDublin.ie>
2025-11-02 15:55:01 +0000

    Added CharDraft and messed around with push stuff

--- Game/Assets/Scripts/PushTestChild.cs
+++ Game/Assets/Scripts/PushTestChild.cs
@@ -5,6 +5,8 @@ public class PushTestChild : MonoBehaviour
     public GameObject parentObject;
     public GameObject childObject;
 
+    public Transform pushLocation;
+
     [SerializeField] private Transform PushCheck;
     [SerializeField] private LayerMask pushLayer;
 
@@ -22,31 +24,48 @@ public class PushTestChild : MonoBehaviour
         //childObject.transform.parent = parentObject.transform;
     }
 
-    void Update()
+    
+    void FixedUpdateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKey(KeyCode.E))
         {
-            if (canPush())
-            {
-                Debug.Log("Yippie");
-                childObject.transform.SetParent(parentObject.transform, worldPositionStays: true);
-            }
-        }
 
-        else
-        {
+            {
+                if (canPush())
+                {
+                Rigidbody2D rb = GetComponent<Rigidbody2D>();
             
+                if (rb != null)
+                {
+                    Vector2 direction = transform.position - pushLocation.transform.position;
+                    float distance = direction.magnitude;
+                
+                    // Calculate force based on distance (stronger when closer)
+                    float forceMultiplier = Mathf.Clamp01(1 - (distance / maxDistance));
+                    float appliedForce = pullForce * forceMultiplier;
+                
+                    // Apply the force smoothly
+                    rb.AddForce(direction.normalized * appliedForce, ForceMode2D.Force);
+                }
+                    
+                }
+            }
+
+
+
         }
     }
+    
 
 
 
     private bool canPush()
     {
         Debug.Log("Player exited the Push-Trigger");
-        return Physics2D.OverlapCircle(PushCheck.position, 0.2f, pushLayer);
+        return Physics2D.OverlapCircle(PushCheck.position, 0.8f, pushLayer);
     }
 
+    /*
     private void FixedUpdate()
     {
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, maxDistance, validLayers);
@@ -69,6 +88,7 @@ public class PushTestChild : MonoBehaviour
             }
         }
     }
+    */
 
 
 
--- Game/Assets/TestPush.cs
+++ Game/Assets/TestPush.cs
@@ -6,7 +6,9 @@ public class TestPush : MonoBehaviour
     [SerializeField] private Transform PushCheck;
     [SerializeField] private LayerMask pushLayer;
 
-    [SerializeField] private GameObject pushBox;
+    public GameObject pushBox;
+
+    public Transform pushLocation;
 
     [SerializeField] private Rigidbody2D rb;
 
@@ -24,9 +26,9 @@ public class TestPush : MonoBehaviour
                 Debug.Log("Is Pushing");
                 //pushBox.transform.position = PushCheck.transform.position;
                 //rb.constraints = RigidbodyConstraints2D.FreezePositionX;
-                Vector2 startPosition = pushBox.transform.position;
-                Vector2 endPosition = PushCheck.transform.position;
-                transform.position = Vector2.Lerp(startPosition, endPosition, 100 * Time.deltaTime);
+                Vector2 startPosition = pushLocation.transform.position;
+                Vector2 endPosition = pushBox.transform.position;
+                transform.position = Vector2.Lerp(startPosition, endPosition, 50 * Time.deltaTime);
             }
 
             else
