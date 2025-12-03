d580562b20a19f7c33dc108b5df733814bd77833
Michael4Camps <C24354791@myTUDublin.ie>
2025-12-03 12:03:10 +0000

    PlayTestLevel

--- Game/Assets/SunBridge.cs
+++ Game/Assets/SunBridge.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using TMPro;
+using System.Net.NetworkInformation;
 
 public class SunBridge : MonoBehaviour
 {
     public GameObject bridgePrefab;   // Assign in Inspector
     public Transform placementPoint;  // Where the bridge should appear
 
+
     public int bridgeCount = 2;
 
     public float timeLeft = 10f;
 
+    // Display/HUD
+    public TextMeshProUGUI sunCount;
+    public GameObject text;
+
     void Update()
     {
+        //sunCount.SetText(bridgeCount.ToString());
+        
+
         if (bridgeCount > 0 && Input.GetKeyDown(KeyCode.Q))
         {
             PlaceBridge();
